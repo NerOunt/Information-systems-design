a6b03d70809ffaaf82d950447b07aecd2386bdfe
Vladislav <->
2026-09-12 11:59:19 +0300

    Пункт 9. Объединил классы через наследование и убрал повтор кода

--- lab1/Student.java
+++ lab1/Student.java
@@ -5,33 +5,18 @@ import com.google.gson.JsonParseException;
 import com.google.gson.Strictness;
 import java.util.Objects;
 
-public class Student {
-    private int studentId;
-    private String lastName;
-    private String firstName;
-    private String patronymic;
+public class Student extends StudentShort {
     private String address;
     private static final int MINIMUM_ELECTIVES = 3;
-    private String phone;
 
     public Student(int studentId, String lastName, String firstName, String patronymic, String address, String phone) {
-        validateStudentId(studentId);
-        validateLastName(lastName);
-        validateFirstName(firstName);
-        validatePatronymic(patronymic);
+        super(studentId, lastName, firstName, patronymic, phone);
         validateAddress(address);
-        validatePhone(phone);
-
-        this.studentId = studentId;
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.patronymic = patronymic;
         this.address = address;
-        this.phone = phone;
     }
 
     public Student(Student student) {
-        this(requireStudent(student).studentId, student.lastName, student.firstName, student.patronymic, student.address, student.phone);
+        this(requireStudent(student).getStudentId(), student.getLastName(), student.getFirstName(), student.getPatronymic(), student.address, student.getPhone());
     }
 
     public Student(String data) {
@@ -44,13 +29,6 @@ public class Student {
 
     private Student(String[] fields) {
         this(Integer.parseInt(fields[0]), fields[1], fields[2], fields[3], fields[4], fields[5]);
-    }
-
-    private static Student requireStudent(Student student) {
-        if (student == null) {
-            throw new IllegalArgumentException("Студент для копирования не должен быть null");
-        }
-        return student;
     }
 
     private static String[] parseStudentData(String data) {
@@ -108,6 +86,114 @@ public class Student {
         return value.getAsString();
     }
 
+    public boolean hasCompletedMinimumElectives(int completedElectives) {
+        if (completedElectives < 0) {
+            throw new IllegalArgumentException("Количество завершенных факультативов не может быть отрицательным");
+        }
+        return completedElectives >= MINIMUM_ELECTIVES;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        validateAddress(address);
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + getStudentId()
+                + "\nФамилия: " + getLastName()
+                + "\nИмя: " + getFirstName()
+                + "\nОтчество: " + getPatronymic()
+                + "\nАдрес: " + address
+                + "\nТелефон: " + getPhone();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!super.equals(object)) {
+            return false;
+        }
+        Student student = (Student) object;
+        return Objects.equals(address, student.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), address);
+    }
+
+    public static void validateAddress(String address) {
+        if (address == null || address.isBlank()) {
+            throw new IllegalArgumentException("Адрес не должен быть пустым");
+        }
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student(1, "Иванов", "Иван", "Иванович", "Москва, ул. Лесная, д. 10", "+79991234567");
+        Student copy = new Student(student);
+        System.out.println("Норма при двух завершенных факультативах: " + student.hasCompletedMinimumElectives(2));
+        System.out.println("Норма при трех завершенных факультативах: " + student.hasCompletedMinimumElectives(3));
+
+        System.out.println("Полная информация:");
+        System.out.println(student);
+
+        System.out.println();
+        System.out.println("Краткая информация:");
+        System.out.println(student.toShortString());
+
+        System.out.println();
+        System.out.println("Студент и копия равны: " + student.equals(copy));
+        copy.setPhone("+79997654321");
+        System.out.println("После изменения телефона равны: " + student.equals(copy));
+
+        StudentShort shortStudent = new StudentShort(student);
+        System.out.println();
+        System.out.println("Объект StudentShort:");
+        System.out.println(shortStudent);
+
+        StudentShort studentAsShort = student;
+        System.out.println();
+        System.out.println("Полный вывод через ссылку StudentShort:");
+        System.out.println(studentAsShort);
+    }
+}
+
+class StudentShort {
+    private int studentId;
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private String phone;
+
+    public StudentShort(int studentId, String lastName, String firstName, String patronymic, String phone) {
+        validateStudentId(studentId);
+        validateLastName(lastName);
+        validateFirstName(firstName);
+        validatePatronymic(patronymic);
+        validatePhone(phone);
+
+        this.studentId = studentId;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.patronymic = patronymic;
+        this.phone = phone;
+    }
+
+    public StudentShort(StudentShort student) {
+        this(requireStudent(student).studentId, student.lastName, student.firstName, student.patronymic, student.phone);
+    }
+
+    protected static StudentShort requireStudent(StudentShort student) {
+        if (student == null) {
+            throw new IllegalArgumentException("Студент для копирования не должен быть null");
+        }
+        return student;
+    }
+
     public int getStudentId() {
         return studentId;
     }
@@ -144,22 +230,6 @@ public class Student {
         this.patronymic = patronymic;
     }
 
-    public boolean hasCompletedMinimumElectives(int completedElectives) {
-        if (completedElectives < 0) {
-            throw new IllegalArgumentException("Количество завершенных факультативов не может быть отрицательным");
-        }
-        return completedElectives >= MINIMUM_ELECTIVES;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        validateAddress(address);
-        this.address = address;
-    }
-
     public String getPhone() {
         return phone;
     }
@@ -171,12 +241,7 @@ public class Student {
 
     @Override
     public String toString() {
-        return "ID: " + studentId
-                + "\nФамилия: " + lastName
-                + "\nИмя: " + firstName
-                + "\nОтчество: " + patronymic
-                + "\nАдрес: " + address
-                + "\nТелефон: " + phone;
+        return toShortString();
     }
 
     public String toShortString() {
@@ -196,18 +261,17 @@ public class Student {
         if (object == null || getClass() != object.getClass()) {
             return false;
         }
-        Student student = (Student) object;
+        StudentShort student = (StudentShort) object;
         return studentId == student.studentId
                 && Objects.equals(lastName, student.lastName)
                 && Objects.equals(firstName, student.firstName)
                 && Objects.equals(patronymic, student.patronymic)
-                && Objects.equals(address, student.address)
                 && Objects.equals(phone, student.phone);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(studentId, lastName, firstName, patronymic, address, phone);
+        return Objects.hash(studentId, lastName, firstName, patronymic, phone);
     }
 
     public static void validateStudentId(int studentId) {
@@ -228,12 +292,6 @@ public class Student {
         validateName(patronymic, "Отчество");
     }
 
-    public static void validateAddress(String address) {
-        if (address == null || address.isBlank()) {
-            throw new IllegalArgumentException("Адрес не должен быть пустым");
-        }
-    }
-
     public static void validatePhone(String phone) {
         if (phone == null || !phone.matches("\\+?[0-9]{10,15}")) {
             throw new IllegalArgumentException("Телефон должен содержать от 10 до 15 цифр, в начале допускается +");
@@ -244,96 +302,5 @@ public class Student {
         if (value == null || value.length() > 50 || !value.matches("\\p{L}+([ '-]\\p{L}+)*")) {
             throw new IllegalArgumentException(fieldName + ": от 1 до 50 символов, только буквы, пробелы, дефисы или апострофы");
         }
-    }
-
-    public static void main(String[] args) {
-        Student student = new Student(1, "Иванов", "Иван", "Иванович", "Москва, ул. Лесная, д. 10", "+79991234567");
-        Student copy = new Student(student);
-        System.out.println("Норма при двух завершенных факультативах: " + student.hasCompletedMinimumElectives(2));
-        System.out.println("Норма при трех завершенных факультативах: " + student.hasCompletedMinimumElectives(3));
-
-        System.out.println("Полная информация:");
-        System.out.println(student);
-
-        System.out.println();
-        System.out.println("Краткая информация:");
-        System.out.println(student.toShortString());
-
-        System.out.println();
-        System.out.println("Студент и копия равны: " + student.equals(copy));
-        copy.setPhone("+79997654321");
-        System.out.println("После изменения телефона равны: " + student.equals(copy));
-
-        StudentShort shortStudent = new StudentShort(student);
-        System.out.println();
-        System.out.println("Объект StudentShort:");
-        System.out.println(shortStudent);
-    }
-}
-
-class StudentShort {
-    private int studentId;
-    private String lastName;
-    private String firstName;
-    private String patronymic;
-    private String phone;
-
-    public StudentShort(int studentId, String lastName, String firstName, String patronymic, String phone) {
-        Student.validateStudentId(studentId);
-        Student.validateLastName(lastName);
-        Student.validateFirstName(firstName);
-        Student.validatePatronymic(patronymic);
-        Student.validatePhone(phone);
-
-        this.studentId = studentId;
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.patronymic = patronymic;
-        this.phone = phone;
-    }
-
-    public StudentShort(Student student) {
-        this(requireStudent(student).getStudentId(), student.getLastName(), student.getFirstName(), student.getPatronymic(), student.getPhone());
-    }
-
-    private static Student requireStudent(Student student) {
-        if (student == null) {
-            throw new IllegalArgumentException("Студент для копирования не должен быть null");
-        }
-        return student;
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    @Override
-    public String toString() {
-        return toShortString();
-    }
-
-    public String toShortString() {
-        String initials = getInitial(firstName) + getInitial(patronymic);
-        return lastName + " " + initials + ", тел. " + phone;
-    }
-
-    private static String getInitial(String name) {
-        return new String(Character.toChars(name.codePointAt(0))) + ".";
     }
 }
